ca727cd7d503c8b25003d290c9292bec8c3ffa05
Refiloe <refiloe.digoamaye@gmail.com>
2019-11-18 14:07:21 +0200

    yes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,60 +2,60 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Footer = props => (
-  <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
-    <ul className="icons">
+  <footer id='footer' style={props.timeout ? { display: 'none' } : {}}>
+    <ul className='icons'>
       <li>
         <a
-          href="https://twitter.com/digoamaye"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="icon fa-twitter"
+          href='https://twitter.com/digoamaye'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='icon fa-twitter'
         >
-          <span className="label">Twitter</span>
+          <span className='label'>Twitter</span>
         </a>
       </li>
       <li>
         <a
-          href="https://www.linkedin.com/in/refiloe-digoamaye/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="icon fa-linkedin"
+          href='https://www.linkedin.com/in/refiloe-digoamaye/'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='icon fa-linkedin'
         >
-          <span className="label">LinkedIn</span>
+          <span className='label'>LinkedIn</span>
         </a>
       </li>
       <li>
         <a
-          href="https://github.com/RefiloeDig"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="icon fa-github"
+          href='https://github.com/RefiloeDig'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='icon fa-github'
         >
-          <span className="label">GitHub</span>
+          <span className='label'>GitHub</span>
         </a>
       </li>
       <li>
         <a
-          href="mailto:refiloe.digoamaye@gmail.com?Subject=Hey!"
-          target="_top"
-          rel="noopener noreferrer"
-          className="icon fa-envelope"
+          href='mailto:refiloe.digoamaye@gmail.com?Subject=Hey!'
+          target='_top'
+          rel='noopener noreferrer'
+          className='icon fa-envelope'
         >
-          <span className="label">Email</span>
+          <span className='label'>Email</span>
         </a>
       </li>
       <li>
         <a
-          href="https://soundcloud.com/weirdblackguy"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="icon fa-soundcloud"
+          href='https://soundcloud.com/weirdblackguy'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='icon fa-soundcloud'
         >
-          <span className="label">SoundCloud</span>
+          <span className='label'>SoundCloud</span>
         </a>
       </li>
     </ul>
-    <p className="copyright"> Made with ✌🏿.</p>
+    <p className='copyright'> Made with ✌🏿.</p>
   </footer>
 )
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
-import '../css/global.css'
+import '../assets/css/global.css'
 
 import '../assets/scss/main.scss'
 
@@ -46,10 +46,6 @@ const Layout = ({ children, location }) => {
       )}
     />
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
